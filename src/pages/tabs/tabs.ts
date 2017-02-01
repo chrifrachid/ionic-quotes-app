050750c8ae449b2e29a382ff9ae10e9697b3677e
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-01 14:37:47 +0100

    Corrects tabs component and display quote's category in view

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from "@angular/core";
 import { FavoritesPage } from "../favorites/favorites";
-import { QuotesPage } from "../quotes/quotes";
+import { LibraryPage } from "../library/library";
 
 @Component({
 	selector: 'page-tabs',
 	template: `
 		<ion-tabs>
 			<ion-tab [root]="favoritesPage" tabTitle="Favorites" tabIcon="star"></ion-tab>
-			<ion-tab [root]="quotesPage" tabTitle="Library" tabIcon="book"></ion-tab>
+			<ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"></ion-tab>
 		</ion-tabs>
 	`
 })
 export class TabsPage {
 	favoritesPage = FavoritesPage;
-	quotesPage = QuotesPage;
+	libraryPage = LibraryPage;
 }
