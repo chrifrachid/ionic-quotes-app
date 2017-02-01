fcefeadf842476f68fd19c036e29118c0220811f
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-01 16:06:46 +0100

    Adds alertController to allow user to confirm adding quote

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController, Alert } from 'ionic-angular';
 import { Quote } from "../../data/quote.interface";
 
 @Component({
@@ -9,13 +9,34 @@ import { Quote } from "../../data/quote.interface";
 export class QuotesPage {
   quotesGroup: { category: string, quotes: Quote[], icon: string }
 
-  constructor(private navParams: NavParams) {}
+  constructor(private navParams: NavParams, private alertCtrl: AlertController) {}
 
   ionViewDidLoad() {
     this.quotesGroup = this.navParams.data;
   }
 
   onAddToFavorite(quote: Quote) {
+    let alert: Alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('OK', quote);
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Alert cancelled');
+          }
+        }
+      ]
+    });
 
+    alert.present();
   }
 }
